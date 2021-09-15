632306b248bc9eed7dd62550cf3374e7fada7021
SerhiiZadorozhnyi <71183966+SerhiiZadorozhnyi@users.noreply.github.com>
2021-09-15 20:54:18 +0300

    Fix code

--- js/color-switch.js
+++ js/color-switch.js
@@ -1,11 +1,11 @@
 const colors = [
-    '#FFFFFF',
-    '#2196F3',
-    '#4CAF50',
-    '#FF9800',
-    '#009688',
-    '#795548',
-  ];
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -17,6 +17,7 @@ const min = 0;
 const buttonStart = document.querySelector("[data-action='start']");
 const buttonStop = document.querySelector("[data-action='stop']");
 const bodyElement = document.querySelector("body");
+
 //Фукция запуска смены цвета фона
 function onButtonStartClick(e) {
   intervalTime = setInterval(() => {
@@ -26,11 +27,13 @@ function onButtonStartClick(e) {
   buttonStart.setAttribute('disabled', false); //Делает клавишу "Start" неактивной при запуске функции смены цвета
   return intervalTime;
 }
+
 //Функция остановки смены цвета фона
 function onButtonStopClick(e) {
   buttonStart.removeAttribute('disabled');
   clearInterval(intervalTime);
 }
+
 //Действия при клике на клавишы
 buttonStart.addEventListener('click', onButtonStartClick)
 buttonStop.addEventListener('click', onButtonStopClick)
